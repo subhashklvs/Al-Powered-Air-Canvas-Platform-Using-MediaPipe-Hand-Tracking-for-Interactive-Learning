212e6908c3edc060692d97445015e1d4203eca97
K.L.V.SAI SUBHASH <subhashklvs@gmail.com>
2026-01-03 09:38:08 +0530

    Add files via upload

---
+++ 4.Al-Powered-Air-Canvas-Platform-Using-MediaPipe-Hand-Tracking-for-Interactive-Learning/HandTrack.py
@@ -0,0 +1,101 @@
+# Import necessary libraries
+import math
+import cv2
+import mediapipe as mp
+import time
+
+# Create a hand detector class
+class handDetector():
+    def __init__(self, mode=False, maxHands=2, modelComplexity=1,
+detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.modelComplex = modelComplexity
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,
+self.modelComplex, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
+
+    # Function to find and draw hands in the image
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms,
+self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    # Function to calculate the distance between two points
+    def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+            length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
+
+    # Function to find hand landmarks and their positions
+    def findPosition(self, img, handNo=0, draw=True):
+        self.lmlist = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                self.lmlist.append([id, cx, cy])
+        return self.lmlist
+
+    # Function to detect finger positions (up or down)
+    def fingersUp(self):
+        fingers = []
+
+        if self.lmlist[self.tipIds[0]][2] < self.lmlist[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        for id in range(1, 5):
+            if self.lmlist[self.tipIds[id]][2] < self.lmlist[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
+
+# Main function
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+
+    detector = handDetector()
+    while True:
+        success, img = cap.read()
+        if not success:
+            break
+        img = detector.findHands(img)
+        lmlist = detector.findPosition(img)
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10, 70),
+cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 255), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+if __name__ == "__main__":
+    main()
+
+cv2.destroyAllWindows()
